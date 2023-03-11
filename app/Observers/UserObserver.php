7d618cf00462b15dadc5c9f663e281fe7e70f6c4
mosabberul <mosabberulislam55@gmail.com>
2023-03-11 15:33:37 +0600

    cache

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\User;
+
+class UserObserver
+{
+    /**
+     * Handle the User "created" event.
+     */
+    public function created(User $user): void
+    {
+        //
+    }
+
+    public function saving(User $user): void
+    {
+        $cacheKey = 'users';
+        cache()->forget($cacheKey);
+    }
+
+    /**
+     * Handle the User "updated" event.
+     */
+    public function updated(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     */
+    public function deleted(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     */
+    public function restored(User $user): void
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     */
+    public function forceDeleted(User $user): void
+    {
+        //
+    }
+}
