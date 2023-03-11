52b03388dd6562fa65bac3729782aa16d6c15157
Nahid Ferdous <65497148+nahidnfr123@users.noreply.github.com>
2023-03-11 17:56:19 +0600

    Imported Observer

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Observers\UserObserver;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
